a9e9423e371a3f6ec0c0d917cdce0f006f547b0d
ohzecodes <mo.ze@outlook.com>
2023-05-05 13:56:07 -0700

    rewrite deepcopy

--- compressString.js
+++ compressString.js
@@ -1,16 +1,4 @@
-/**
-Given an array of characters chars, compress it using the following algorithm:
-Begin with an empty string s. For each group of consecutive repeating characters in chars:
-If the group's length is 1, append the character to s.
-Otherwise, append the character followed by the group's length.
-The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
-After you are done modifying the input array, return the new length of the array.
-You must write an algorithm that uses only constant extra space.
- * @param {character[]} charArray
- * @return {number}
- * 
- * 
- */
+
 /*
 var compress = (charArray) => {
   let count = 1;
@@ -87,41 +75,19 @@ var compress = (givenCharArray) => {
   }
   // split the number above 10 to "1","0"  after converting it to string using the join()
   compressedArray = compressedArray.join("").split("");
-  //   deepcopy
+
   givenCharArray = deepcopyBtoA(compressedArray, givenCharArray);
-
-  // remove extra Elements
-  givenCharArray.splice(compressedArray.length);
-
-  return givenCharArray.length;
+  return givenCharArray;
 };
 /**
  * @param {[]} source the array you want to copy from
  * @param {[]} destination the array you want to copy to
  */
 const deepcopyBtoA = (source, destination) => {
-  for (let i = 0; i < source.length; i++) {
-    destination[i] = source[i];
-  }
+  destination.splice(0,destination.length)
+ source.forEach((e) => {
+  destination.push(e)
+ });
   return destination;
 };
 
-// console.log(compressString(["a", "a", "b", "b", "c", "c", "c"]));
-
-// console.log(
-//   compressString([
-//     "a",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//     "b",
-//   ])
-// );
